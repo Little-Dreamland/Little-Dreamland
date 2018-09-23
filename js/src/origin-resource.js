6963d1c8276227b651164dca8bfa58dfa8514e32
ROYIANS <z1294686101@gmail.com>
2018-09-23 16:21:35 +0800

    666

--- js/src/origin-resource.js
+++
@@ -1,8 +0,0 @@
-var resource = [
-    {ID:0 , title:"" , url:"" , command :""},
-    {ID:1024 , title:"您的电影" , url:"" , command :"vidade408"},
-    {ID:20180109001 , title:"权力的游戏 S07E01" , url:"https://apis.tianxianle.com/youku/?id=pJ6crnidnpoo00oY1Hltk5traGppa2cO0O0O&type=migu" , command :"HU%Az!YeW95#!DLOcrBM"},
-    {ID:20180108003 , title:"敦刻尔克(原声版) Dunkirk" , url:"http://api.zuilingxian.com/jiexi.php?url=https://v.qq.com/x/cover/eiltk86r8o001mf.html" , command :"u$SMI#T@uaX#^^L3v#o@"},
-    {ID:20180108002 , title:"魔卡少女樱 透明牌篇 第1集" , url:"http://api.zuilingxian.com/jiexi.php?url=https://v.qq.com/x/cover/enddu63r2aa01la.html" , command :"&TrMHp!sUs&fl^C@"},
-    {ID:20180108001 , title:"羞羞的铁拳" , url:"http://api.zuilingxian.com/jiexi.php?url=https://v.qq.com/x/cover/jpjl78i91mx05bu.html" , command:"test"}  
-]
